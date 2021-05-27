be58aaa2219fb98c2a546680293d7badb34c675a
Sidharth Sethi <Sidharth.sherry@gmail.com>
2021-05-27 22:08:49 +0530

    cleaning of extra stuff

--- Covid_Vaccination.py
+++ Covid_Vaccination.py
@@ -5,7 +5,6 @@ from datetime import datetime, timedelta
 age = int(input("Enter Age "))
 zipcode = input("Enter Pin Code ")
 pin = zipcode.split()
-#pin = ["110001"]
 no_days = int(input("Enter Days "))
 flag = 'Y'
 
@@ -19,7 +18,8 @@ while 1:
         for enter_date in date_format:
             url = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode={}&date={}".format(find, enter_date)
 
-            requirements = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'}
+            requirements = {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'}
 
             final = requests.get(url, headers=requirements)
 
@@ -27,31 +27,31 @@ while 1:
                 file_json = final.json()
                 flag2 = False
                 if file_json["centers"]:
-                    if(flag.lower() == "y"):
+                    if flag.lower() == "y":
 
                         for place in file_json["centers"]:
                             for vaccine in place["sessions"]:
-                                if(vaccine["min_age_limit"]<=age and vaccine["available_capacity"]>0):
+                                if vaccine["min_age_limit"] <= age and vaccine["available_capacity"] > 0:
                                     print("")
-                                    print("PinCode :- " ,pin)
+                                    print("PinCode :- ", pin)
                                     print("Date :- {}".format(enter_date))
                                     print("Location :- ", place["name"])
-                                    print("Block :-",place["block_name"])
-                                    print("Price :-",place["fee_type"])
-                                    print("Vaccine :-",vaccine["available_capacity"])
+                                    print("Block :-", place["block_name"])
+                                    print("Price :-", place["fee_type"])
+                                    print("Vaccine :-", vaccine["available_capacity"])
 
-                                    if (vaccine["vaccine"] != ''):
-                                        print("Type :- ",vaccine["vaccine"])
+                                    if vaccine["vaccine"] != '':
+                                        print("Type :- ", vaccine["vaccine"])
                                         print("")
 
-                                    i = i+1
+                                    i = i + 1
                                 else:
                                     pass
                     else:
                         pass
                 else:
                     print("x.x No Response")
-    if (i==0):
+    if i == 0:
         print("No Vaccine... Try Again Later ")
     else:
         print("Search End")
